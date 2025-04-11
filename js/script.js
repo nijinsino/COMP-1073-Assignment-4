08a31e8f00ec3daf3a89c8ce525732f2020864fd
Nijin Sino <nijinsino@gmail.com>
2025-04-11 15:09:59 -0400

    added contents for data search

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ function searchMovie() {
         'X-RapidAPI-Key': '59f758066dmshfec404f6bc92e9fp11273djsn2848b5bcaa3b',
         'X-RapidAPI-Host': 'movie-database-imdb-alternative.p.rapidapi.com'
       }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const resultDiv = document.getElementById("result");
+      resultDiv.innerHTML = '';
+
+      if (data.Search) {
+        data.Search.forEach(movie => {
+          resultDiv.innerHTML += `
+            <div style="margin-bottom: 20px;">
+              <img src="${movie.Poster}" alt="${movie.Title}" width="200" /><br>
+              <strong>${movie.Title} (${movie.Year})</strong>
+            </div>
+          `;
+        });
+      } else {
+        resultDiv.innerHTML = "No results found.";
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      document.getElementById("result").innerText = "Error fetching movie data.";
+    });
+}
